0013341e2948d2124f31f7c2ddae72bd6f067807
Ian Silva Santos <ian.santos@sptech.school>
2024-07-20 17:21:21 -0300

    feat: Iniciando a estrutura dos microsserviços de card e loan

---
+++ card/src/main/java/com/microservices/card/CardApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardApplication.class, args);
+	}
+
+}
