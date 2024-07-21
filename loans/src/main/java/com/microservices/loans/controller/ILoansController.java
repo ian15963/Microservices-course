077fcfb1ae69f9a26793dee2861873ad0f83028f
Ian Silva Santos <ian.santos@sptech.school>
2024-07-21 18:58:11 -0300

    feat: Finalizando a implementação do CRUD no controller

--- loans/src/main/java/com/microservices/loans/controller/ILoansController.java
+++ loans/src/main/java/com/microservices/loans/controller/ILoansController.java
@@ -1,4 +1,122 @@
 package com.microservices.loans.controller;
+import com.microservices.loans.dto.ErrorResponseDto;
+import com.microservices.loans.dto.LoansDto;
+import com.microservices.loans.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Loans in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE loan details"
+)
+@Validated
 public interface ILoansController {
+
+    @Operation(
+            summary = "Create Loan REST API",
+            description = "REST API to create new loan inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PostMapping("/create")
+    ResponseEntity<ResponseDto> createLoan(@RequestParam
+                                           @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                           String mobileNumber);
+
+    @Operation(
+            summary = "Fetch Loan Details REST API",
+            description = "REST API to fetch loan details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetch")
+    ResponseEntity<ResponseDto> fetchLoan(@RequestParam
+                                          @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                          String mobileNumber);
+
+    @Operation(
+            summary = "Update Loan Details REST API",
+            description = "REST API to update loan details based on a loan number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("/update")
+    ResponseEntity<ResponseDto> updateLoans(@Valid @RequestBody LoansDto loansDto);
+
+    @Operation(
+            summary = "Delete Loan Details REST API",
+            description = "REST API to delete Loan details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @DeleteMapping("/delete")
+    ResponseEntity<ResponseDto> deleteLoan(@RequestParam
+                                           @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                           String mobileNumber);
+
 }
--- loans/src/main/java/com/microservices/loans/controller/impl/LoansControllerImpl.java
+++ loans/src/main/java/com/microservices/loans/controller/impl/LoansControllerImpl.java
@@ -1,10 +1,50 @@
 package com.microservices.loans.controller.impl;
 
+import com.microservices.loans.constants.LoansConstants;
 import com.microservices.loans.controller.ILoansController;
+import com.microservices.loans.dto.LoansDto;
+import com.microservices.loans.dto.ResponseDto;
+import com.microservices.loans.service.ILoansService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/loans")
+@RequiredArgsConstructor
 public class LoansControllerImpl implements ILoansController {
+
+    private final ILoansService service;
+
+    @Override
+    public ResponseEntity<ResponseDto> createLoan(String mobileNumber) {
+        service.createLoan(mobileNumber);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> fetchLoan(String mobileNumber) {
+        LoansDto dto = service.fetchLoan(mobileNumber);
+        return ResponseEntity.ok(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> updateLoans(LoansDto loansDto) {
+        boolean isUpdated = service.updateLoan(loansDto);
+        if(isUpdated){
+            return ResponseEntity.ok(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        }
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_UPDATE));
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> deleteLoan(String mobileNumber) {
+        boolean isDeleted = service.deleteLoan(mobileNumber);
+        if(isDeleted){
+            return ResponseEntity.ok(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        }
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_DELETE));
+    }
 }
