35fc8ce90e94c597eca9b1bf8fadae1f86d4da39
Ian Silva Santos <ian.santos@sptech.school>
2024-07-31 14:49:11 -0300

    feat: Iniciando a configuração do actuator no microsserviço de Account, e alterando o record de AccountContact para classe.

--- account/src/main/java/com/microservices/account/dto/AccountContactInfoDto.java
+++ account/src/main/java/com/microservices/account/dto/AccountContactInfoDto.java
@@ -1,10 +1,18 @@
 package com.microservices.account.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties("accounts")
-public record AccountContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class AccountContactInfoDto {
+
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
