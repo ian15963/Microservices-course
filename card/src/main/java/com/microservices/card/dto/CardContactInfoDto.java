fbd558f1153209e0c3c4eba4059848ac66305dae
Ian Silva Santos <ian.santos@sptech.school>
2024-07-31 14:54:10 -0300

    feat: Iniciando a configuração do actuator para o microsserviço de card, e trocando o record de CardContact para classe.

--- card/src/main/java/com/microservices/card/dto/CardContactInfoDto.java
+++ card/src/main/java/com/microservices/card/dto/CardContactInfoDto.java
@@ -1,10 +1,19 @@
 package com.microservices.card.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties("cards")
-public record CardContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardContactInfoDto {
+
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
+
 }
