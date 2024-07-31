7fd9a42e9eb3cedb8c52a5ff60d34f1544cd3de7
Ian Silva Santos <ian.santos@sptech.school>
2024-07-31 14:52:13 -0300

    feat: Iniciando a configuração do actuator no microsserviço de loans, e alterando a record de LoansContact para classe.

--- loans/src/main/java/com/microservices/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/microservices/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,18 @@
 package com.microservices.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties("loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSuport) {
+public class LoansContactInfoDto {
+
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSuport;
 }
