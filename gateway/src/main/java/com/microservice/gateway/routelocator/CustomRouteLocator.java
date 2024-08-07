f02f950496e92faf5d5025f3fbcfc605dd7e4f9b
Ian Silva Santos <ian.santos@sptech.school>
2024-08-07 10:44:19 -0300

    feat: Habilitando configuração global default para os circuitbreakers.

--- gateway/src/main/java/com/microservice/gateway/routelocator/CustomRouteLocator.java
+++ gateway/src/main/java/com/microservice/gateway/routelocator/CustomRouteLocator.java
@@ -1,5 +1,10 @@
 package com.microservice.gateway.routelocator;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -33,6 +38,13 @@ public class CustomRouteLocator {
                                 .addResponseHeader("X-Response-DateTime", LocalDateTime.now().toString()))
                         .uri("lb://LOANS"))
                 .build();
+    }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
     }
 
 }
