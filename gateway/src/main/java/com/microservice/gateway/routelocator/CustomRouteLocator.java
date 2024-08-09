49f04992a022190d6ef105ca7048d6aace8768de
Ian Silva Santos <ian.santos@sptech.school>
2024-08-09 11:52:43 -0300

    feat: Configurando rateLimiter pattern no gateway utilizando o Redis.

--- gateway/src/main/java/com/microservice/gateway/routelocator/CustomRouteLocator.java
+++ gateway/src/main/java/com/microservice/gateway/routelocator/CustomRouteLocator.java
@@ -5,11 +5,14 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -35,7 +38,9 @@ public class CustomRouteLocator {
                         .uri("lb://CARDS"))
                 .route(p -> p.path("/microservice/loan/**")
                         .filters(f -> f.rewritePath("/microservice/loan/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-DateTime", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-DateTime", LocalDateTime.now().toString())
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())))
                         .uri("lb://LOANS"))
                 .build();
     }
@@ -47,4 +52,14 @@ public class CustomRouteLocator {
                 .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
     }
 
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(1,1,1);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver(){
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+                .defaultIfEmpty("anonymous");
+    }
 }
